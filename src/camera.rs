0338806c5a75b96605d637adacfd28f4cbbb6ecb
Artoghrul Gahramanli <artogahr@gmail.com>
2024-05-28 20:33:21 +0200

    Ch 12.2: Positioning and Orienting the Camera

--- src/camera.rs
+++ src/camera.rs
@@ -1,4 +1,5 @@
 use core::f64;
+use std::f32::consts::PI;
 
 use crate::color::write_color;
 use crate::hittable::*;
@@ -20,6 +21,13 @@ pub struct Camera {
     pixel_delta_v: Vec3,
     pixel_samples_scale: f64,
     max_depth: u32,
+    vfov: f64,
+    lookfrom: Point3,
+    lookat: Point3,
+    vup: Vec3,
+    v: Vec3,
+    u: Vec3,
+    w: Vec3, // Camera frame basis vectors
 }
 
 impl Camera {
@@ -32,16 +40,24 @@ impl Camera {
                 image_height
             }
         };
-        // Calculate the vectors across the horizontal and down the vertical viewport edges.
-        let focal_length: f64 = 1.0;
-        let viewport_height: f64 = 2.0;
+        // Calculate the vectors across the horizontal and down the vertical viewport edges. sg
+        let vfov: f64 = 20.0;
+        let theta = degrees_to_radians(vfov);
+        let h = f64::tan(theta / 2.0);
+        let lookfrom = Point3::new(-2.0, 2.0, 1.0);
+        let lookat = Point3::new(0.0, 0.0, -1.0);
+        let focal_length: f64 = (lookfrom - lookat).length();
+        let viewport_height: f64 = 2.0 * h * focal_length;
         let viewport_width: f64 = viewport_height * ((image_width as f64) / (image_height as f64));
-        let viewport_u = Vec3::new(viewport_width, 0.0, 0.0);
-        let viewport_v = Vec3::new(0.0, -viewport_height, 0.0);
-        let center = Point3::new(0.0, 0.0, 0.0);
+        let vup = Vec3::new(0.0, 1.0, 0.0);
+        let w = (lookfrom - lookat).normalized();
+        let u = (vup.cross(w)).normalized();
+        let v = w.cross(u);
+        let viewport_u = viewport_width * u;
+        let viewport_v = viewport_height * -v;
+        let center = lookfrom;
         // Calculate the location of the upper left pixel.
-        let viewport_upper_left =
-            center - Vec3::new(0.0, 0.0, focal_length) - viewport_u / 2.0 - viewport_v / 2.0;
+        let viewport_upper_left = center - (focal_length * w) - viewport_u / 2.0 - viewport_v / 2.0;
         let pixel_delta_u = viewport_u / image_width as f64;
         let pixel_delta_v = viewport_v / image_height as f64;
         let pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
@@ -57,6 +73,13 @@ impl Camera {
             pixel00_loc,
             pixel_samples_scale: 1.0 / samples_per_pixel as f64,
             max_depth,
+            vfov,
+            lookfrom,
+            lookat,
+            vup,
+            v,
+            u,
+            w,
         }
     }
 
@@ -124,4 +147,7 @@ impl Camera {
 
         Ray::new(ray_origin, ray_direction)
     }
+}
+fn degrees_to_radians(degrees: f64) -> f64 {
+    degrees * PI as f64 / 180.0
 }
